5afeb0f7580995c22f41dfdf85ab6b7744e50b8a
Vistillo <130785837+Vistillo@users.noreply.github.com>
2023-09-06 18:55:13 +0200

    Reto #0 - Java

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/Victor.java
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/Victor.java
@@ -3,9 +3,7 @@ public class Reto_0 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int i;
-		
-		for(i=1;i<101;i++) {
+		for(int i=1;i<101;i++) {
 			int num=3;
 			int num2=5;
 			if(i%num==0 && i%num2==0 && i>5) {
